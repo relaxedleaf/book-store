81e2c5e7f2a068465c181cc091c36c00d0ac3332
Guanghui Li <liguanghui.cs@gmail.com>
2019-01-30 10:12:31 -0500

    fixed bugs so that whenever a product is added, no error appears

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ if Rails.env.production?
       config.aws_credentials = {
         :access_key_id     => ENV['S3_ACCESS_KEY'],
         :secret_access_key => ENV['S3_SECRET_KEY'],
-        :region            => ENV["AWS_REGION"]
+        :region            => ENV['AWS_REGION']
       }
       config.aws_bucket      =  ENV['S3_BUCKET']
       config.storage         =  :aws
