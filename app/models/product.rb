db727ba8b2d529faffcc768fc9a9c143d666ed2c
Guanghui Li <liguanghui.cs@gmail.com>
2019-01-30 00:05:41 -0500

    finished all the part 2 and 3.1

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+    mount_uploader :image_url, ImageUploader
     validates :title, :description, :image_url, presence:true
     validates :title, uniqueness: true
     validates :image_url, allow_blank: true, format: {
