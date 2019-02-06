abdbc83c1cc9b8934437cb34ba364b1bd701897b
Guanghui Li <liguanghui.cs@gmail.com>
2019-02-05 23:41:57 -0500

    add the flash message when the user visit the store more than five times

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,17 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    if session[:counter] == nil
+      session[:counter] = 0
+    
+    else
+        session[:counter] += 1
+
+        if session[:counter] > 5
+          flash.now[:index] = "You have visited the page #{session[:counter]} times".pluralize{session[:counter]} + " without buying anything"
+          render :index
+        
+        end
+      end
+    end
   end
-end
