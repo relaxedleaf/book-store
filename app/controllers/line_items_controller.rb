579598e87c34ffe645e2c54fadd572ea54ecc3b1
Guanghui Li <liguanghui.cs@gmail.com>
2019-02-18 20:53:35 -0500

    Finished F3 and F4

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -41,7 +41,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_index_url }
-        format.js
+        format.js { @current_item = @line_item }
         # format.json { render :show,
         #   status: :created, location: @line_item }
         format.json { redirect_to cart_path(@line_item.cart)}
