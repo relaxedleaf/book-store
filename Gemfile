876765542c4ca8ff5db455a578f3d591fad2cb53
Guanghui Li <liguanghui.cs@gmail.com>
2019-02-20 21:47:50 -0500

    finished final step up for pushing to heroku

--- Gemfile
+++ Gemfile
@@ -40,7 +40,7 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'json', '2.0.2'
 # Use Redis adapter to run Action Cable in production
-# gem 'redis', '~> 4.0'
+gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   resources :carts
   resources :products
   get 'search', to: 'store#search'
+  mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
